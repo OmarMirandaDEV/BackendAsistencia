f02c263df33c4a902599421c03c146bbd3d31525
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2026-04-02 20:02:55 -0600

    Fix gitignore and add project files

---
+++ app/routes/course_routes.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.models.course import Course
+from app.schemas.course_schema import CourseCreate
+from app.dependencies.database import get_db
+from app.dependencies.auth import get_current_teacher
+
+router = APIRouter(prefix="/courses", tags=["Courses"])
+
+
+# CREAR CURSO
+@router.post("/")
+def create_course(
+    data: CourseCreate,
+    db: Session = Depends(get_db),
+    current_teacher = Depends(get_current_teacher)
+):
+    new_course = Course(
+        course_name=data.course_name,
+        teacher_id=current_teacher["id"]
+    )
+
+    db.add(new_course)
+    db.commit()
+    db.refresh(new_course)
+
+    return {
+        "message": "Curso creado correctamente",
+        "course": {
+            "id": new_course.course_id,
+            "name": new_course.course_name
+        }
+    }
